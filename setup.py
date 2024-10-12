9ee7b038e50e673d0155dbb0c760886a00bdaaf8
Mohd-Farhan-Khan <goldenstone447@gmail.com>
2024-10-12 14:50:02 +0530

    setup format

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import find_packages, setup
 from typing import List
 
-HYPHEN_E_DOT = '-e .'
+HYPHEN_E_DOT = "-e ."
+
+
 def get_requirements(file_path: str) -> List[str]:
-    """ 
+    """
     this function will return the list of requirements
     """
     requirements = []
@@ -16,11 +18,12 @@ def get_requirements(file_path: str) -> List[str]:
 
     return requirements
 
+
 setup(
-    name = 'ML-Project',
-    version = '0.0.1',
-    author = 'Farhan',
-    author_email = 'goldenstone447@gmail.com',
-    packages = find_packages(),
-    install_requires = get_requirements('requirements.txt')
+    name="ML-Project",
+    version="0.0.1",
+    author="Farhan",
+    author_email="goldenstone447@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt"),
 )

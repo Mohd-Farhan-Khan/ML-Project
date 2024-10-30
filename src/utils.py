ab321b5db1aabb6f77dd81684eb663b5736f7eee
Mohd Farhan Khan <goldenstone447@gmail.com>
2024-10-30 16:21:36 +0530

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,7 @@ import pandas as pd
 import dill
 import pickle
 from sklearn.metrics import r2_score
-from sklearn.model_selection import GridSearchCV
+# from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
